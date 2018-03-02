8ad4295ef07bf0261adb0adbf1adc371bdaa3ccb
robbine <hansong1983@gmail.com>
2018-03-02 15:33:34 +0800

    fix dim bug

--- model/connectionist_network_pointer.py
+++ model/connectionist_network_pointer.py
@@ -11,22 +11,6 @@ from model.tf_util import *
 import numpy as np
 from tensorflow.python.ops import ctc_ops as ctc
 
-def _get_probs_from_state_and_memory(state, memory, matrix,
-    batch_size, ctx_dim, max_ctx_len):
-    """Performs softmax(state . matrix . memory)
-        Inputs:
-            state: The state of size [batch_size, 1, d]
-            memory: The memory of size [batch_size, max_ctx_len, 2 * rnn_size]
-            matrix: The matrix of size [d, 2 * rnn_size]
-
-        Outputs:
-            A tensor of size [batch_size, max_ctx_len]
-    """
-    sW = multiply_tensors(state, matrix) # size = [batch_size, 1, 2 * rnn_size]
-    sWM = tf.matmul(memory, tf.transpose(sW, perm=[0, 2, 1])) # size = [batch_size, max_ctx_length, 1]
-    sWM = tf.reshape(sWM, [batch_size, max_ctx_len])
-    return tf.nn.softmax(sWM, dim=1) # size = [batch_size, max_ctx_len]
-
 def connectionist_network_pointer(options, ctx, qst, sparse_span_iterator, sq_dataset, keep_prob,
     sess, batch_size, use_dropout, ctx_lens):
     """Runs a stochastic answer pointer to get start/end span predictions
@@ -56,7 +40,6 @@ def connectionist_network_pointer(options, ctx, qst, sparse_span_iterator, sq_da
         sm = tf.nn.softmax(Qw, dim=1) # size = [batch_size, max_qst_length]
         s = tf.matmul(tf.reshape(sm, [batch_size, 1, max_qst_len])
             , qst) # size = [batch_size, 1, 2 * rnn_size]
-        ctx_transpose = tf.transpose(ctx, perm=[0, 2, 1]) # size = [batch_size, 2 * rnn_size, max_ctx_len]
 
         lstm = create_cudnn_lstm(ctx_dim,
             sess, options, "lstm", keep_prob,
@@ -64,20 +47,11 @@ def connectionist_network_pointer(options, ctx, qst, sparse_span_iterator, sq_da
         state_h = s
         state_c = s
         n_hidden = options.rnn_size
-        W = tf.get_variable("W", shape=[ctx_dim, ctx_dim], dtype=tf.float32)
 
         weights_classes = tf.Variable(tf.truncated_normal([n_hidden, 3],
                                                      stddev=np.sqrt(2.0 / n_hidden)))
         biases_classes = tf.Variable(tf.zeros([3]))
-
-        beta = _get_probs_from_state_and_memory(s, ctx, W,
-            batch_size, ctx_dim, max_ctx_len) # size = [batch_size, max_ctx_len]
-        x = tf.reshape(
-                tf.matmul(ctx_transpose,
-                    tf.reshape(beta, [batch_size, max_ctx_len, 1])) # size = [batch_size, 2 * rnn_size, 1]
-            , [batch_size, 1, ctx_dim]) # size = [batch_size, 1, 2 * rnn_size]
-
-        s, state_h, state_c = run_cudnn_lstm(x, keep_prob, options,
+        s, state_h, state_c = run_cudnn_lstm(ctx, keep_prob, options,
             lstm, batch_size, use_dropout,
             initial_state_h=state_h, initial_state_c=state_c) # size(s) = [batch_size, 1, rnn_size]
         fb_h1rs = [tf.squeeze(t) for t in tf.split(s, max_ctx_len, axis=1)] # size(fb_h1rs) = [batch_size, rnn_size]
