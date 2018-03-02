12cf003eb75c85ac3a33eb80753f712ae059c627
robbine <hansong1983@gmail.com>
2018-03-02 16:36:51 +0800

    fix bug

--- model/connectionist_network_pointer.py
+++ model/connectionist_network_pointer.py
@@ -56,7 +56,7 @@ def connectionist_network_pointer(options, ctx, qst, sparse_span_iterator, sq_da
         fb_h1rs = [tf.squeeze(t) for t in tf.split(s, max_ctx_len, axis=1)] # size(fb_h1rs) = [batch_size, rnn_size]
         logits = [tf.matmul(t, weights_classes) + biases_classes for t in fb_h1rs]
         logits3d = tf.stack(logits)
-        loss = tf.reduce_mean(ctc.ctc_loss(sparse_span_iterator, logits3d, ctx_lens))
+        loss = ctc.ctc_loss(sparse_span_iterator, logits3d, ctx_lens)
 
         ####Evaluating
         #predictions = tf.to_int32(ctc.ctc_beam_search_decoder(logits3d, ctx_lens)[0][0])
